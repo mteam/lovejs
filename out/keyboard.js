3448c429d7eab99ff577348341f4cecc1923b02f
Michal Miskernik <michal.miskernik@gmail.com>
2012-03-24 19:21:29 +0100

    keyboard: key names

--- out/keyboard.js
+++ out/keyboard.js
@@ -1,5 +1,5 @@
 (function() {
-  var Base, Events, Keyboard,
+  var Base, Events, Keyboard, i, keys, letter, _len, _ref,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = Object.prototype.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
@@ -27,14 +27,14 @@
     Keyboard.prototype.keyDown = function(event) {
       var key;
       key = event.keyCode;
-      if (!this.isDown(key)) this.trigger('keyDown', key);
+      if (!this.isDown(key)) this.trigger('keyDown', this.getName(key));
       return this.keys[key] = true;
     };
 
     Keyboard.prototype.keyUp = function(event) {
       var key;
       key = event.keyCode;
-      if (this.isDown(key)) this.trigger('keyUp', key);
+      if (this.isDown(key)) this.trigger('keyUp', this.getName(key));
       return this.keys[key] = false;
     };
 
@@ -42,8 +42,65 @@
       return !!this.keys[key];
     };
 
+    Keyboard.prototype.getName = function(code) {
+      return keys[code] || code;
+    };
+
     return Keyboard;
 
   })(Base);
+
+  keys = {
+    8: "backspace",
+    9: "tab",
+    13: "return",
+    16: "shift",
+    17: "ctrl",
+    18: "alt",
+    20: "capslock",
+    32: " ",
+    33: "pageup",
+    34: "pagedown",
+    35: "end",
+    36: "home",
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+    45: "insert",
+    46: "delete",
+    91: "meta",
+    93: "menu",
+    106: "kp*",
+    107: "kp+",
+    109: "kp-",
+    110: "kp.",
+    111: "kp/",
+    186: ";",
+    187: "=",
+    188: ",",
+    189: "-",
+    190: ".",
+    191: "/",
+    219: "[",
+    220: "\\",
+    221: "]",
+    222: "'"
+  };
+
+  _ref = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  for (i = 0, _len = _ref.length; i < _len; i++) {
+    letter = _ref[i];
+    keys[i + 65] = letter;
+  }
+
+  for (i = 0; i <= 9; i++) {
+    keys[i + 48] = i;
+    keys[i + 96] = "kp" + i;
+  }
+
+  for (i = 1; i <= 12; i++) {
+    keys[i + 111] = "f" + i;
+  }
 
 }).call(this);
--- src/keyboard.coffee
+++ src/keyboard.coffee
@@ -16,7 +16,7 @@ class Keyboard extends Base
 		key = event.keyCode
 
 		unless @isDown key
-			@trigger 'keyDown', key
+			@trigger 'keyDown', @getName key
 
 		@keys[key] = yes
 
@@ -24,9 +24,64 @@ class Keyboard extends Base
 		key = event.keyCode
 
 		if @isDown key
-			@trigger 'keyUp', key
+			@trigger 'keyUp', @getName key
 
 		@keys[key] = no
 
 	isDown: (key) ->
 		!!@keys[key]
+
+	getName: (code) ->
+		keys[code] or code
+
+keys =
+	8: "backspace"
+	9: "tab"
+	13: "return"
+	16: "shift"
+	17: "ctrl"
+	18: "alt"
+	20: "capslock"
+	32: " "
+	33: "pageup"
+	34: "pagedown"
+	35: "end"
+	36: "home"
+	37: "left"
+	38: "up"
+	39: "right"
+	40: "down"
+	45: "insert"
+	46: "delete"
+	91: "meta"
+	93: "menu"
+	106: "kp*"
+	107: "kp+"
+	109: "kp-"
+	110: "kp."
+	111: "kp/"
+	186: ";"
+	187: "="
+	188: ","
+	189: "-"
+	190: "."
+	191: "/"
+	219: "["
+	220: "\\"
+	221: "]"
+	222: "'"
+
+# a-z
+for letter, i in 'abcdefghijklmnopqrstuvwxyz'.split ''
+	keys[i + 65] = letter
+
+# numbers
+for i in [0..9]
+	keys[i + 48] = i
+	keys[i + 96] = "kp#{i}"
+
+# f keys
+for i in [1..12]
+	keys[i + 111] = "f#{i}"
+
+
