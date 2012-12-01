3d8ffea407e2f29b7771a050e3f3d6f9b3372699
Michal Miskernik <michal.miskernik@gmail.com>
2012-12-02 00:23:09 +0100

    love.keyboard

--- test/all/index.js
+++ test/all/index.js
@@ -2,3 +2,4 @@ require('./graphics');
 require('./shims');
 require('./timer');
 require('./events');
+require('./keyboard');
---
+++ test/all/keyboard.js
@@ -0,0 +1,51 @@
+var expect = require('expect.js'),
+    sham = require('sham'),
+    love = { keyboard: require('../../lib/keyboard') };
+
+describe('love.keyboard', function() {
+  afterEach(function() {
+    love.keyboard._events = {};
+    love.keyboard.pressed = {};
+  });
+
+  var events = {
+    p: { keyCode: 80 },
+    7: { keyCode: 55 },
+    kp6: { keyCode: 102 },
+    f7: { keyCode: 118 },
+    down: { keyCode: 40 },
+    esc: { keyCode: 27 }
+  };
+
+  it('should trigger events', function() {
+    var pressed = sham.spy().called(),
+        released = sham.spy().called();
+
+    love.keyboard.on('pressed', pressed);
+    love.keyboard.on('released', released);
+
+    love.keyboard.down(events.p);
+    love.keyboard.up(events.p);
+
+    pressed.check();
+    released.check();
+  });
+
+  it('should give correct key name', function() {
+    var pressed = sham.spy();
+    love.keyboard.on('pressed', pressed);
+
+    for (var name in events) {
+      pressed.args(name);
+      love.keyboard.down(events[name]);
+    }
+  });
+
+  it('should remember whether key is pressed', function() {
+    expect(love.keyboard.isDown('p')).to.be(false);
+    love.keyboard.down(events.p);
+    expect(love.keyboard.isDown('p')).to.be(true);
+    love.keyboard.up(events.p);
+    expect(love.keyboard.isDown('p')).to.be(false);
+  });
+});
