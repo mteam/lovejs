477f2eddb4be78390580007b0c1f9e44fb6b876d
Michal Miškerník <michal.miskernik@gmail.com>
2012-12-07 02:41:21 -0500

    love.rect: collisions

--- test/all/rect.js
+++ test/all/rect.js
@@ -19,6 +19,11 @@ describe('love.rect', function() {
     expect(rect.right).to.be(140);
     expect(rect.bottom).to.be(280);
   });
+  
+  it('.AABB should work', function() {
+     var rect = love.rect.aabb(20, 30, 40, 60);
+     expect(String(rect)).to.be('rect[20;30 20x30]');
+  });
 
   it('#move should move it', function() {
     var rect = love.rect(20, 30, 400, 500);
@@ -38,5 +43,74 @@ describe('love.rect', function() {
     expect(String(rect)).to.be('rect[20;30 600x700]');
     expect(rect.right).to.be(620);
     expect(rect.bottom).to.be(730);
+  });
+  
+  describe('#collides', function() {
+    it('should return false when not colliding', function() {
+      var a = love.rect(10, 10, 100, 100),
+          b = love.rect(200, 300, 10, 10);
+      
+      expect(a.collides(b)).to.be(false);
+      expect(b.collides(a)).to.be(false);
+    });
+    
+    it('should return true when colliding', function() {
+      var a = love.rect(10, 10, 100, 100),
+          b = love.rect(50, 70, 100, 100);
+      
+      expect(a.collides(b)).to.be(true);
+      expect(b.collides(a)).to.be(true);
+    });
+    
+    it('should return true when a rect is inside another', function() {
+      var a = love.rect(10, 10, 100, 100),
+          b = love.rect(30, 40, 10, 10);
+      
+      expect(a.collides(b)).to.be(true);
+      expect(b.collides(a)).to.be(true);
+    });
+  });
+  
+  describe('#includes', function() {
+    it('should return false when not inside', function() {
+      var rect = love.rect(10, 10, 100, 100);
+      
+      expect(rect.includes(0, 0)).to.be(false);
+      expect(rect.includes(110.1, 110.1)).to.be(false);
+    });
+    
+    it('should return true when inside', function() {
+      var rect = love.rect(10, 10, 100, 100);
+      
+      expect(rect.includes(20, 20)).to.be(true);
+      expect(rect.includes(10, 10)).to.be(true);
+      expect(rect.includes(110, 110)).to.be(true);
+    });
+  });
+  
+  describe('#overlap', function() {
+    it('should return null when not overlapping', function() {
+      var a = love.rect(10, 10, 100, 100),
+          b = love.rect(200, 300, 10, 10);
+          
+      expect(a.overlap(b)).to.be(null);
+      expect(b.overlap(a)).to.be(null);
+    });
+    
+    it('should return rect when overlapping', function() {
+      var a = love.rect(10, 10, 100, 100),
+          b = love.rect(50, 70, 100, 100);
+      
+      expect(String(a.overlap(b))).to.be('rect[50;70 60x40]');
+      expect(String(b.overlap(a))).to.be('rect[50;70 60x40]');
+    });
+    
+    it('should return rect when inside', function() {
+      var a = love.rect(10, 10, 100, 100),
+          b = love.rect(30, 40, 10, 10);
+      
+      expect(String(a.overlap(b))).to.be(String(b));
+      expect(String(b.overlap(a))).to.be(String(b));
+    });
   });
 });
