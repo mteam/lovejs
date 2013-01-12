c1a9aa5840c6dbc61a4f03b2daea835fff7a0bf6
Michal Miskernik <michal.miskernik@gmail.com>
2013-01-12 17:58:05 +0100

    helpers.to.array, improved helpers tests

--- test/all/helpers.js
+++ test/all/helpers.js
@@ -1,9 +1,9 @@
 var expect = require('expect.js'),
-    love = { helpers: require('../../lib/helpers') };
+    helpers = require('../../lib/helpers');
 
 describe('love.helpers', function() {
   it('is.number', function() {
-    var isn = love.helpers.is.number;
+    var isn = helpers.is.number;
 
     expect(isn(123)).to.be(true);
     expect(isn(123.45)).to.be(true);
@@ -16,12 +16,30 @@ describe('love.helpers', function() {
   });
 
   it('is.string', function() {
-    var iss = love.helpers.is.string;
+    var iss = helpers.is.string;
 
     expect(iss('foo')).to.be(true);
 
     expect(iss(123)).to.be(false);
     expect(iss(null)).to.be(false);
     expect(iss(undefined)).to.be(false);
+  });
+
+  it('is.func', function() {
+    var isf = helpers.is.func;
+
+    function foo() {}
+    var bar = function() {};
+
+    expect(isf(foo)).to.be(true);
+    expect(isf(bar)).to.be(true);
+  });
+
+  it('to.array', function() {
+    var fn = function() {
+      expect(helpers.to.array(arguments)).to.be.an('array');
+    };
+
+    fn(1, 2, 3);
   });
 });
